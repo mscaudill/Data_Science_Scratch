730d3970c01dd552faa810a242b86e2dfe927920
giladmeir <mscaudill@gmail.com>
2016-02-13 15:45:01 -0800

    working on code to draw state boundaries

---
+++ Ch12_K_nearest_neighbors/plot_state_bounds.py
@@ -0,0 +1,11 @@
+""" This script plots the state boundaries specified in the text file
+state_boundaries.txt """
+
+import re
+
+# Open the text file for reading #
+##################################
+
+with open(state_borders.txt,'rb') as f:
+    for line in f:
+        if 
