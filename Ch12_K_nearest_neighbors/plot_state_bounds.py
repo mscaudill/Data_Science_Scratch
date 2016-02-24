85369eed41f40fd072c417c4830cdccd0327668d
giladmeir <mscaudill@gmail.com>
2016-02-23 20:10:08 -0800

    state names and boundaries put into defaultdict

--- Ch12_K_nearest_neighbors/plot_state_bounds.py
+++ Ch12_K_nearest_neighbors/plot_state_bounds.py
@@ -2,10 +2,26 @@
 state_boundaries.txt """
 
 import re
+from collections import defaultdict
 
 # Open the text file for reading #
 ##################################
-
-with open(state_borders.txt,'rb') as f:
+state_borders = defaultdict(list)
+with open('state_boundaries.txt','rb') as f:
+    current_state = ''
+    
     for line in f:
-        if 
+
+        if re.search('state name', line):
+            # get the state name
+            state_name = re.search(r'(state name=")(\w+)',line).group(2)
+            state_borders[state_name]
+            current_state = state_name
+
+        elif re.search('point', line):
+            # get the latitude and longitude of this line
+            latitude = float(re.search(r'(lat=")([\d.-]+)', line).group(2))
+            longitude = float(re.search(r'(lng=")([\d.-]+)',line).group(2))
+            state_borders[current_state].append([latitude,longitude])
+            
+print state_borders
