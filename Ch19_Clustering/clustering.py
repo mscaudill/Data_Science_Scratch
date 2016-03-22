da41e9d401c814f84875d625870ac2420979e154
giladmeir <mscaudill@gmail.com>
2016-03-22 11:29:00 -0700

    complete k-means clustering of sample data and sample png image

--- Ch19_Clustering/clustering.py
+++ Ch19_Clustering/clustering.py
@@ -1,16 +1,24 @@
 """
-# of clusters defined in advance
+In this module we examine k-means and hierarchal clustering. We take two
+sample data sets and cluster using k m-means. This algorithm assumes:
+we know the # of clusters defined in advance. It follows these steps:
 1. start with a set of kmeans (a point in d-dimensions)
 2. assign each point to the mean to which it is closest
 3. if no point's assignment has changed stop because we've converged
 4. if some points' assignment has changed recompute means and iterate
+Then we examine hierarchal clustering. This algorithm merges clustersthat
+are nearest neighbors until only one cluster is left. We then unmerge the
+clusters in reverse order so we get k clusters. We have written but not
+implemented this algorithm because it is very ineffecient. Will use the Ward
+hierarchal clustering in Scipy in the future.
 """
 from DS_Scratch.Ch4_Linear_Algebra import squared_distance, vector_mean
 
 # KMeans Clustering algorithm #
 ###############################
-class KMeans(object):
+class KMeans:
     """ performs k-means clustering """
+    
     def __init__(self, k):
         # assign # of clusters and means to initialized clusterer object
         self.k = k
@@ -18,9 +26,13 @@ class KMeans(object):
 
     def classify(self, input):
         """ returns the index of the cluster with a vector-mean closest to
-            the input"""
-        return min(range(self.k), 
-                    key=lambda i: squared_distance(input, self.means[i]))
+        the input """
+        def helper(cluster_index):
+            """ calculates the squared distance between input and
+            cluster vector mean """
+            return squared_distance(input, self.means[cluster_index])
+                
+        return min(range(self.k), key=helper)
 
     def train(self, inputs):
         # Choose k random points as the initial means
@@ -63,39 +75,67 @@ def is_leaf(cluster):
     return len(cluster) == 1
 
 def get_children(cluster):
-     """ returns the two children of this cluster if it's a merged cluster;
-        and raises exception if this is a leaf cluster """
-        if is_leaf(cluster):
-            raise TypeError("a leaf cluster has no children")
-        else:
-            return cluster[1]
+    """ returns the two children of this cluster if it's a merged cluster;
+     and raises exception if this is a leaf cluster """
+    if is_leaf(cluster):
+        raise TypeError("a leaf cluster has no children")
+    else:
+        return cluster[1]
 
 def get_values(cluster):
     """ returns the value in this cluster (if its a leaf cluster) or all the
-        values in the leaf clusters below it (if its not) """
-        if is_leaf(cluster):
-            return cluster
-        else:
-            return [value for child in get_children(cluster)
-                          for value in get_values(child)
+    values in the leaf clusters below it (if its not) """
+    if is_leaf(cluster):
+        return cluster
+    else:
+        return [value for child in get_children(cluster)
+                          for value in get_values(child)]
 
-def cluster_distance(cluster1, cluster2, distance_metric=max):
+def cluster_distance(cluster1, cluster2, distance_metric=min):
     """ compute all pairwise distances of points in cluster1 and cluster2
-        and apply distance_metric to the resulting list """  
+    and apply distance_metric to the resulting list """  
     return distance_metric([distance(input1, input2) 
                             for input1 in get_values(cluster1)
-                            for input2 in get_values(cluster2)]
+                            for input2 in get_values(cluster2)])
 
 def get_merge_order(cluster):
     """ smaller numbers are later merges so when unmerging we get the
         smallest merge order first """
-        if is_leaf(cluster):
-            return float('inf')
-        else:
-            return cluster[0]
+    if is_leaf(cluster):
+        return float('inf')
+    else:
+        return cluster[0]
 
-# Now create main algorithm
+# Now create main  bottom-up algorithm
+def bottom_up_cluster(inputs, distance_metric):
+    # start with each input being a leaf cluster (1 tuple)
+    clusters = [(input,) for input in inputs]
 
+    # while we have more than one cluster perform the following
+    while len(clusters) > 1:
+        
+        # compute cluster distances
+        def key_helper(clusters_tuple):
+            """ resturns the distance between two clusters """
+            return cluster_distance(cluster_tup[1], cluster_tup[2], 
+                                    distance_metric)
+
+        # find two closest clusters
+        c1, c2 = min([(cluster1, cluster2) 
+                      for i, cluster1 in enumerate(clusters)
+                      for cluster2 in clusters[:i]], key=key_helper)
+        
+        # remove them from the list of clusters since they are to be merged
+        clusters = [c for c in clusters if c != c1 and c != c2]
+
+        # merge the clusters using merge_order = # of clusters left
+        merged_cluster = (len(clusters), [c1,c2])
+
+        # add the merge to clusters
+        clusters.append(merged_cluster)
+
+    # when only one cluster left, return it
+    return clusters[0]
 
 if __name__ == '__main__':
     
@@ -148,7 +188,7 @@ if __name__ == '__main__':
     
     # Clustering Colors #
     #####################
-    image_path = r'/home/giladmeir/Pictures/test.jpg'
+    image_path = r'/home/giladmeir/Pictures/4071.png'
     orig_image = mpimg.imread(image_path)
 
     # The image is a list of list of list where the innermost list is [r g
@@ -157,14 +197,14 @@ if __name__ == '__main__':
     pixels = [pixel for row in orig_image for pixel in row]
 
     # provide these to the clusterer
-    clusterer = KMeans(5)
-    clusterer.train(pixels)
+    im_clusterer = KMeans(5)
+    im_clusterer.train(pixels)
 
     # now we just need to recolor each pixel with one of the 5 colors
     def recolor(pixel):
-        cluster = clusterer.classify(pixel)
-        return clusterer.means[cluster]
-
+        im_cluster = im_clusterer.classify(pixel)
+        return im_clusterer.means[im_cluster]
+    print im_clusterer.means
     new_image = [[recolor(pixel) for pixel in row] for row in orig_image]
 
     fig2 = plt.figure(2, figsize=(14,4))
@@ -174,8 +214,7 @@ if __name__ == '__main__':
     ax4 = fig2.add_subplot(122)
     ax4.imshow(new_image)
     ax4.axis('off')
-    
-    
+     
     plt.show()
 
     
