6e92f66608d9b6f2d52e1b2cee5f935c35a240fc
giladmeir <mscaudill@gmail.com>
2016-03-23 12:56:12 -0700

    n-grams complete, working on grammars

--- Ch20_Natural_Language_Processing/n_gram_models.py
+++ Ch20_Natural_Language_Processing/n_gram_models.py
@@ -3,10 +3,14 @@ Here we are going to build a statistical model of language from a given
 document. The model works by using a given word in a document and choosing
 n-pairs of words surrounding the given word and then forming sentences. We
 will do this for the article "What is data science" on the Oreilly website
-so just as in Ch9 we will use beautiful soup to parse the words and periods
+so just as in Ch9 we will use beautiful soup to parse the words and periods.
+We will then explore Grammars which are rules for generating acceptable
+sentences.
 """
 
 from bs4 import BeautifulSoup
+from collections import defaultdict
+import random
 import requests
 import re
 
@@ -43,3 +47,58 @@ for prev, current in bigrams:
     transitions[prev].append(current)
 
 # now we can generate sentences
+def bigram_sentence_generator():
+    current = "."
+    result = []
+    while True:
+        # look up in transitions dict the next word
+        next_word_candidates = transitions[current]
+        # make the new current word a random choice from the candidates
+        current = random.choice(next_word_candidates)
+        # append to results to build a sentence
+        result.append(current)
+        if current == ".": 
+            bigram_sentence = " ".join(result)
+            return bigram_sentence
+
+print "BIGRAM SENTENCE ------------"
+print bigram_sentence_generator()
+print '\n'
+
+# Trigrams #
+############
+# The bigram sentence is gibberish, we can make it less so by considering
+# three consecutive words called a trigram
+trigrams = zip(document, document[1:], document[2:])
+trigram_transitions = defaultdict(list)
+starts = []
+
+for prev, current, next in trigrams:
+    # here we need to keep track of starts because we need to get the next
+    # two words so we can't just make a random choice from the next word
+    # candidates as we did before
+    if prev == '.':
+        starts.append(current)
+
+    trigram_transitions[(prev,current)].append(next)
+
+# now we can generate trigram sentences
+def trigram_sentence_generator():
+    current = random.choice(starts)
+    prev = '.'
+    result = [current]
+
+    while True:
+        next_word_candidates = trigram_transitions[(prev, current)]
+        next_word = random.choice(next_word_candidates)
+
+        prev, current = current, next_word
+        result.append(current)
+
+        if current == '.':
+            return " ".join(result)
+
+print "TRIGRAM SENTENCE --------------"
+print trigram_sentence_generator()
+
+
