e8b330d7dfe7721a2e225c9c8bdf1d4a815bbf43
giladmeir <mscaudill@gmail.com>
2016-03-29 14:03:22 -0700

    betweeness centrality metric complete

--- Ch21_Network_Analysis/network_analysis.py
+++ Ch21_Network_Analysis/network_analysis.py
@@ -51,7 +51,6 @@ positions = nx.get_node_attributes(G,'pos')
 G.add_edges_from(friendships)
 # draw the graph placing the nodes at the positions
 nx.draw(G,positions)
-plt.show()
 
 # Breadth First Search Algorithm #
 ##################################
@@ -96,16 +95,53 @@ def shortest_paths(graph, start_node, end_node):
     shortest_paths = [path for path in paths if len(path) <=min_path_length]
     return shortest_paths
 
-print shortest_paths(users,0,4)
-
+# Calulate all Shortest Paths #
+###############################
 # now we are ready to calculate for each node in our network the shortest
 # paths to all other nodes in the network
+
+# add shortest path and betweeness centrality keys to users
 for user in users:
     user["shortest_paths"] = []
+    user['betweeness_centrality'] = 0.0
 
+# for each start and end node call shortest_paths to get all the shortest
+# paths and append to users
 for start_node, _ in enumerate(users):
     for end_node, _ in enumerate(users):
         if start_node != end_node:
             users[start_node]['shortest_paths'].append(shortest_paths(users,
             start_node, end_node))
-        
+
+# Betweeness Centrality #
+#########################
+# for each pair start/end nodes 
+for start_node,_ in enumerate(users):
+    for end_node, _ in enumerate(users):
+        if start_node < end_node:
+            # get all the paths, remember the nodes start at 0
+            paths = users[start_node]['shortest_paths'][end_node-1]
+            # get the number of paths
+            num_paths = len(paths)
+            # the contibution for each id in the path should be 1/n paths
+            contribution = 1/float(num_paths)
+            # add the contribution to each id in each path
+            for path in paths:
+                for id in path:
+                    if id not in [start_node, end_node]:
+                        users[id]['betweeness_centrality'] += contribution
+
+# Let the size of each node be the centrality metric, 50 is arbitrary, size
+# is normally 300, what matters here is size difference between nodes
+sizes = [50*user['betweeness_centrality'] for user in users]
+
+fig = plt.figure(2)
+fig.suptitle("Betweeness-Centrality of User-Friends Network",fontsize=14, 
+              fontweight='bold')
+# get the nodes and attributes dictionary
+positions = nx.get_node_attributes(G,'pos')
+# add the friendships as edges to the graph
+G.add_edges_from(friendships)
+# draw the graph placing the nodes at the positions
+nx.draw(G,positions,node_size=sizes)
+plt.show()
